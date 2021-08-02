0d74ba3013d694c9bf1d5a4c79a2a6f47b564c0f
Jessica Jones <daddehjones@gmail.com>
2021-08-02 16:31:58 -0400

    Create food_trucks_that_sell method.

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,10 @@ class Event
   def food_truck_names
     @food_trucks.map { |truck| truck.name  }
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |truck|
+      truck.check_stock(item) > 0
+    end
+  end
 end
