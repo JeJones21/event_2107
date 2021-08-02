3b1b278921d035a197390239f74f2b26291e5408
Jessica Jones <daddehjones@gmail.com>
2021-08-02 15:27:06 -0400

    Create readable attributes test.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe Item do
       expect(@item1).to be_a(Item)
       expect(@item2).to be_a(Item)
     end
+
+    it "has readable attributes" do
+      expect(@item2.name).to eq("Apple Pie (Slice)")
+      expect(@item2.price).to eq(2.50)
+    end
   end
 end
