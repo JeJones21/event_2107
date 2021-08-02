01b22986337a6680caa10a0f1aa83ab211ffd4fa
Jessica Jones <daddehjones@gmail.com>
2021-08-02 15:58:55 -0400

    Instantiate Event object.

---
+++ spec/event_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+
+RSpec.describe Event do
+  before :each do
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  describe 'Object Creation' do
+    it "exists" do
+      expect(@event).to be_a(Event)
+    end
+  end 
+end
