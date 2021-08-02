61eacb5bb99d8d9ae2cdb4fef4ad93c55c6422c8
Jessica Jones <daddehjones@gmail.com>
2021-08-02 15:35:28 -0400

    Create readable attributes test.

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe FoodTruck do
     it "exists" do
       expect(@food_truck).to be_a(FoodTruck)
     end
+
+    it "has readable attributes" do
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck.inventory).to eq({})
+    end
   end
+
+
+
 end
