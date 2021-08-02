663094e78b30693c77e14702085ddfa3b96686ee
Jessica Jones <daddehjones@gmail.com>
2021-08-02 16:52:06 -0400

    Create ugly total_inventory method using each.

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,21 @@ class Event
       truck.check_stock(item) > 0
     end
   end
+
+  def total_inventory
+    total_inventory = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, quantity|
+        if total_inventory[item].nil?
+          total_inventory[item] = {
+            quantity: 0,
+            food_trucks: []
+          }
+        end
+        total_inventory[item][:quantity] += quantity
+        total_inventory[item][:food_trucks] << truck
+      end
+    end
+    total_inventory
+  end
 end
